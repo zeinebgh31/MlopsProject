b948d5b5ce65a1fe9a7d7f27810427bebad52c82
zeinebgh31 <163097246+zeinebgh31@users.noreply.github.com>
2026-01-10 17:11:17 +0100

    Update indexation.py

--- src/indexation.py
+++ src/indexation.py
@@ -4,33 +4,30 @@ import faiss
 import time
 from sentence_transformers import SentenceTransformer
 
-# 1. Chargement des données
+
 print("Chargement du catalogue...")
 df = pd.read_csv('catalogue_nettoye_10k.csv')
 sentences = df['designation'].astype(str).tolist()
 
-# 2. Choix du modèle (Le "cerveau" de votre IA)
-# 'all-MiniLM-L6-v2' est le meilleur compromis vitesse/précision pour l'anglais
+
 print("Chargement du modèle NLP...")
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# 3. Génération des Embeddings (Transformation en vecteurs)
+
 print(f"Génération des vecteurs pour {len(sentences)} lignes... (cela peut prendre un moment)")
 start_time = time.time()
 embeddings = model.encode(sentences, show_progress_bar=True)
 print(f"Vecteurs générés en {time.time() - start_time:.2f} secondes.")
 
-# 4. Création de l'index FAISS
-# On utilise IndexFlatL2 pour une recherche exacte par distance Euclidienne
-dimension = embeddings.shape[1] # Pour ce modèle, c'est 384
+
+dimension = embeddings.shape[1] 
 index = faiss.IndexFlatL2(dimension)
 index.add(np.array(embeddings).astype('float32'))
 
-# 5. Sauvegarde de l'index et des données (Indispensable pour Docker plus tard)
+
 faiss.write_index(index, "index.faiss")
 print("Index FAISS sauvegardé avec succès !")
 
-# --- TEST RAPIDE ---
 query = "heavy duty industrial faucet"
 print(f"\nTest de recherche pour : '{query}'")
 query_vector = model.encode([query])
